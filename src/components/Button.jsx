09ca42128d4a807c4cc0036a5585e534d88b8a1e
Claudinei Bido <claudinei.bido@extern.dkbcodefactory.com>
2021-01-11 17:10:04 +0100

    extending styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,32 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const StyledButton = styled.button`
-  padding: 1rem 1.5rem;
-  font-size: 1.8rem;
-  outline: none;
-  border: 1px solid ${({primary}) => primary ? '#000' : '#fff'};
-  color: ${props => props.primary ? '#000' : '#fff'};
-  background-color: ${props => props.primary ? '#fff' : '#000'};
+    background-color: #fff;
+    color: palevioletred;
+    font-size: 1rem;
+    margin: 1rem;
+    padding: .5rem 1rem;
+    border-radius: 3px;
+
+    ${({primary})=> primary && css`
+        background-color: palevioletred;
+        color: #fff;
+        box-shadow: 0 0.5rem 1.5rem rgba(0,0,0, 0.2)
+    `}
 `;
 
+const SuperButton = styled(StyledButton) `
+    font-size: 3rem;
+
+`
+
 const Button = ({ primary, children }) => {
-  return <StyledButton primary={primary}>{children}</StyledButton>;
+  return (
+    <>
+    <StyledButton primary={primary}>{children}</StyledButton>
+    <SuperButton primary={primary}>{children}</SuperButton>
+    </>
+
+  );
 };
 export default Button;
