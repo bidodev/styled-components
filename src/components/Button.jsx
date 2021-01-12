b43b0b6295827010756e198226eda2fd61c75c0a
Claudinei Bido <claudinei.bido@extern.dkbcodefactory.com>
2021-01-12 10:31:42 +0100

    fadein

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,32 +1,52 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
+import {Wrapper, FadeIn} from "../components/styled/index";
+
 
 const StyledButton = styled.button`
-    background-color: #fff;
-    color: palevioletred;
-    font-size: 1rem;
-    margin: 1rem;
-    padding: .5rem 1rem;
-    border-radius: 3px;
+  background-color: #fff;
+  color: palevioletred;
+  font-size: 1rem;
+  margin: ${({ margin }) => margin || "2rem"};
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  animation: 5s ${FadeIn} ease-in;
 
-    ${({primary})=> primary && css`
-        background-color: palevioletred;
-        color: #fff;
-        box-shadow: 0 0.5rem 1.5rem rgba(0,0,0, 0.2)
+  ${({ primary }) =>
+    primary &&
+    css`
+      background-color: palevioletred;
+      color: #fff;
+      box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.2);
     `}
+
+  ${({ showBorderOnHover }) =>
+    showBorderOnHover &&
+    `
+        
+        &:hover {
+            border: 1px solid red;
+        }
+        
+        `}
+
+  .someClass {
+      color: blue;
+  }
 `;
 
-const SuperButton = styled(StyledButton) `
-    font-size: 3rem;
-
-`
+const SuperButton = styled(StyledButton)`
+  font-size: 3rem;
+`;
 
 const Button = ({ primary, children }) => {
   return (
-    <>
-    <StyledButton primary={primary}>{children}</StyledButton>
-    <SuperButton primary={primary}>{children}</SuperButton>
-    </>
-
+    <Wrapper>
+      <StyledButton showBorderOnHover={false} margin={"5rem"} primary={primary}>
+        {children}
+        <p className="someClass">Hello</p>
+      </StyledButton>
+      <SuperButton primary={primary}>{children}</SuperButton>
+    </Wrapper>
   );
 };
 export default Button;
---
+++ src/components/styled/index.js
@@ -0,0 +1,17 @@
+import styled, { keyframes } from "styled-components";
+
+export const Wrapper = styled.section `
+    margin-top: 2rem;
+    border: 1px solid palevioletred;
+
+`
+export const FadeIn = keyframes `
+0% {
+    opacity: 0:
+}
+
+100% {
+    opacity: 1;
+}
+
+`
